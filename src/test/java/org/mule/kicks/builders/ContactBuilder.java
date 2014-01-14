9f8cceb7f82f2a3b6a44aa32028d8080af062af4
Javier Casal <javier.casal@mulesoft.com>
2014-01-14 11:09:16 -0300

    added copy constructor to builder

--- src/test/java/org/mule/kicks/builders/ContactBuilder.java
+++ src/test/java/org/mule/kicks/builders/ContactBuilder.java
@@ -5,15 +5,24 @@ import java.util.Map;
 
 public class ContactBuilder {
 
-	private Map<String, String> fields = new HashMap<String, String>();
+	private Map<String, String> fields;
+
+	public ContactBuilder() {
+		this.fields = new HashMap<String, String>();
+	}
+	
+	public ContactBuilder(ContactBuilder original) {
+		this.fields = original.fields;
+	}
 
 	public static ContactBuilder aContact() {
 		return new ContactBuilder();
 	}
 	
 	public ContactBuilder with(String field, String value) {
-		this.fields.put(field, value);
-		return this;
+		ContactBuilder contactCopy = new ContactBuilder(this);
+		contactCopy.fields.put(field, value);
+		return contactCopy;
 	}
 	
 	public Map<String, String> build() {
