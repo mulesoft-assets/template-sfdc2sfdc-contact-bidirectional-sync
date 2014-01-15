44b7573ae9257c73c7fc9bed8d7c8f5d36969450
Javier Casal <javier.casal@mulesoft.com>
2014-01-15 16:39:31 -0300

    semaphore code refactor

--- src/test/java/org/mule/kicks/semaphore/Semaphore.java
+++ src/test/java/org/mule/kicks/semaphore/Semaphore.java
@@ -2,24 +2,45 @@ package org.mule.kicks.semaphore;
 
 import java.io.Serializable;
 
+import org.apache.log4j.Logger;
 import org.mule.api.store.ObjectStore;
 import org.mule.api.store.ObjectStoreException;
 
 
 public class Semaphore {
-	private static final String SEMAPHORE_KEY = "";
+	
+	private static final Logger log = Logger.getLogger(Semaphore.class.getName());
+	private static final String SEMAPHORE_KEY = "status";
+	private static final String AVAILABLE_STATUS = "IDLE";
+	private static final String BUSY_STATUS = "BUSY";
 	private ObjectStore<Serializable> os;
 
-	public synchronized boolean red() throws ObjectStoreException {
-		String status = (String) os.retrieve(SEMAPHORE_KEY);
-		if (status.equals("idle")) {
-			os.store(SEMAPHORE_KEY, "bussy");
-			return true;
+	public synchronized boolean red() {
+		String status;
+		
+		try {
+			status = (String) os.retrieve(SEMAPHORE_KEY);
+			if (status.equals(AVAILABLE_STATUS)) {
+				os.store(SEMAPHORE_KEY, BUSY_STATUS);
+				return true;
+			}
+		} catch (ObjectStoreException e) {
+			log.error("The ObjectStore is not available");
 		}
+		
 		return false;
 	}
 
-	public synchronized void green() {
+	public synchronized boolean green() {
+		
+		try {
+			os.store(SEMAPHORE_KEY, AVAILABLE_STATUS);
+			return true;
+		} catch (ObjectStoreException e) {
+			log.error("The ObjectStore is not available");
+		}
+		
+		return false;
 	}
 
 	public ObjectStore<Serializable> getOs() {
