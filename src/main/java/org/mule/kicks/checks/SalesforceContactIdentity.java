15f6fdce6a278f04c29b6cb96cf76bae34bd9ccc
Javier Casal <javier.casal@mulesoft.com>
2014-01-17 16:02:44 -0300

    created Salesforce Contact identity using a has function

---
+++ src/main/java/org/mule/kicks/checks/SalesforceContactIdentity.java
@@ -0,0 +1,26 @@
+package org.mule.kicks.checks;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class SalesforceContactIdentity {
+	
+	private final static List<String> contactIdentifierFields = Arrays.asList("FirstName", "LastName", "Email");
+
+	
+	public static boolean areEqual(Map<String, String> contact1, Map<String, String> contact2) {
+		return hashCode(contact1) == hashCode(contact2);
+	}
+	
+	public static int hashCode(Map<String, String> contact) {
+		int result = 17;
+		for (String contactIdentifierField : contactIdentifierFields) {
+			if (contact.containsKey(contactIdentifierField)) {
+				final String fieldValue = contact.get(contactIdentifierField);
+				result = 37 * result + fieldValue.hashCode();
+			}
+		}
+		return result;
+	}
+}
