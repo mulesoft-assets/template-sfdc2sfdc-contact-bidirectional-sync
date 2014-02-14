abeedd5fd010f0ce41ba11e272676dfd2ab22ceb
Javier Casal <javier.casal@mulesoft.com>
2014-02-14 14:16:24 -0300

    added workaround to make the integration run despite bug EE-3825

---
+++ src/main/java/org/mule/kicks/workaround/BatchAutoreferenceWorkaround.java
@@ -0,0 +1,32 @@
+package org.mule.kicks.workaround;
+
+import org.mule.DefaultMuleEvent;
+import org.mule.DefaultMuleMessage;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.tck.MuleTestUtils;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.mule.util.UUID;
+
+public class BatchAutoreferenceWorkaround extends AbstractMessageTransformer {
+
+	@Override
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+		Object payload = message.getPayload();
+		MuleMessage newMessage = new DefaultMuleMessage(payload, muleContext);
+		newMessage.setCorrelationId(null);
+		((DefaultMuleMessage) message).setUniqueId(UUID.getUUID());
+		
+		MuleEvent event = null;
+		try {
+			event = MuleTestUtils.getTestEvent("", MessageExchangePattern.REQUEST_RESPONSE, muleContext);
+			event.setMessage(newMessage);
+		} catch (Exception e) {
+			logger.error("Error produced while creating a new Mule Event");
+		}
+		return event;
+	}
+
+}
